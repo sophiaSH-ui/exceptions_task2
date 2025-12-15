32b73cc90abb36473abacd91af0480f30a01ca46
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2025-12-15 15:25:30 +0200

    Add usings

--- ex-task2/ex-task2/Form1.cs
+++ ex-task2/ex-task2/Form1.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
 namespace ex_task2
 {
     public partial class Form1 : Form
