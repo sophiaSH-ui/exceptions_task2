2bd935737a80c58fec1ecd3c83c96f0c61eaf9e4
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2025-12-15 15:30:55 +0200

    Add regex and path to folder (with photo)

--- ex-task2/ex-task2/Form1.cs
+++ ex-task2/ex-task2/Form1.cs
@@ -16,7 +16,22 @@ namespace ex_task2
             button1.Text = "Обробити папку photo";
         }
 
-       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string targetDir = @"C:\Users\Asus\Desktop\exceptions-task2\ex_task2\ex_task2\bin\Debug\net9.0\photo";
+
+            if (!Directory.Exists(targetDir))
+            {
+                MessageBox.Show($"Папку не знайдено за шляхом:\n{targetDir}", "Помилка шляху", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string[] files = Directory.GetFiles(targetDir);
+            Regex regexExtForImage = new Regex(@"^((bmp)|(gif)|(tiff?)|(jpe?g)|(png))$", RegexOptions.IgnoreCase);
+
+            
+
+        }
     }
 }
 
