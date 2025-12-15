acc7ddb89d9d464bedeac87fbe5d9c78be62086a
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2025-12-15 15:28:46 +0200

    Add method Form

--- ex-task2/ex-task2/Form1.cs
+++ ex-task2/ex-task2/Form1.cs
@@ -5,13 +5,18 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
+
 namespace ex_task2
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            button1.Text = "Обробити папку photo";
         }
+
+       
     }
 }
+
